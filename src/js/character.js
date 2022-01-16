1719a1a0a4d64ab1b409c417cba922ea16ce5566
Patrick Dolan <dolanp1992@gmail.com>
2022-01-16 11:52:22 -0800

    add methods to add data to all constructor fields
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>

--- src/js/character.js
+++ src/js/character.js
@@ -1,19 +1,43 @@
 class Character {
-  constructor(characterName) {
+  constructor() {
     this.characterName = "";
     this.playerName = "";
     this.alignment = "";
     this.race = {};
-    this.class = {};
+    this.characterClass = {};
     this.hitpoints = 0;
     this.abilityScores = [];
     this.armorClass = 0;
     this.equipment = {};
   }
-  addAbilityScores(array) {
-    this.abilityScores = array;
+  // Add methods for constructor
+  addCharacterName(characterName) {
+    this.characterName = characterName;
+  }
+  addPlayerName(playerName) {
+    this.playerName = playerName;
+  }
+  addAlignment(alignment) {
+    this.alignment = alignment;
   }
   addRace(race) {
     this.race = race;
   }
+  addCharacterClass(characterClass) {
+    this.characterClass = characterClass;
+  }
+  addHitPoints(hitpoints) {
+    this.hitpoints = hitpoints;
+  }
+  addAbilityScores(array) {
+    this.abilityScores = array;
+  }
+  addArmorClass(armorClass) {
+    this.armorClass = armorClass;
+  }
+  addEquipment(equipment) {
+    this.equipment = equipment;
+  }
+  // Shortcut Methods 
+  
 }
