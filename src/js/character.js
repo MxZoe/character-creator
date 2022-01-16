34c45d044d3001b5418205d834bcc6aed89af56b
Patrick Dolan <dolanp1992@gmail.com>
2022-01-16 11:30:39 -0800

    create character.js file and add class constructor
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>

---
+++ src/js/character.js
@@ -0,0 +1,15 @@
+class Character {
+  constructor(characterName) {
+    this.characterName = "";
+    this.playerName = "";
+    this.alignment = "";
+    this.race = {};
+    this.class = {};
+    this.hitpoints = 0;
+    this.abilityScores = {};
+    this.armorClass = 0;
+  }
+  getAbilityScores() {
+    
+  }
+}
