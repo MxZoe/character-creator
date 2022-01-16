3bd3a0bf25a2d9718673e431e8124a7f718f50b0
Patrick Dolan <dolanp1992@gmail.com>
2022-01-16 11:59:28 -0800

    add import statement for character.js and some testing code

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Character from "./js/character";
+
+let character = new Character();
+console.log(character);
