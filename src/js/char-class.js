3b028dd413878ce1f128b4af65324e6b1f54dd62
Zoe Weinstein <mxzweinstein@gmail.com>
2023-04-02 18:53:32 -0700

    Updates

--- src/js/char-class.js
+++
@@ -1,9 +0,0 @@
-export default class CharClass {
-  constructor(name, hitDie, proficiencyChoice, hasProficiency, savingThrows){
-    this.name = name;
-    this.hit_die = hitDie;
-    this.proficiencyChoice = proficiencyChoice;
-    this.hasProficiency = hasProficiency;
-    this.savingThrows = savingThrows;
-  }
-}
